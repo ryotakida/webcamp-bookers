9f699f8e144688e38706c16baa78693db3b52359
ryotakida <uulee5521@gmail.com>
2021-09-09 13:34:11 +0000

    バリデーション実装完了

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -18,21 +18,28 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id), notice:"successfully"
+    @books = Book.all
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book.id), notice:"successfully"
+    else
+      render :index
+    end
   end
 
   def destroy
     book = Book.find(params[:id])
     book.destroy
-    redirect_to books_path
+    redirect_to books_path, notice:"successfully"
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
     redirect_to book_path(book.id), notice:"successfully"
+    else
+      render :edit
+    end
   end
 
   private
